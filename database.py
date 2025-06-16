e5eeda7d0803e8a4500e60a19e97f4aba88ca039
Pavan <pavanmadhup@gmail.com>
2025-06-16 15:56:16 +0800

    Prelim

---
+++ database.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from bson import ObjectId  # Still required for MongoDB _id handling
+
+
+# Create a client connection
+client = MongoClient("mongodb://localhost:27017")
+db = client["car_database"]
+dataset = db["cars"]
+
+# Serialize ObjectId and document into a dictionary
+def convert(car) :
+    return {
+        "id": str(car["_id"]),
+        "brand": car["brand"],
+        "model": car["model"],
+        "colour" : car["colour"],
+        "year": car["year"]
+    }
---
+++ main.py
@@ -0,0 +1,9 @@
+
+from fastapi import FastAPI, HTTPException
+from structure import Car
+from database import dataset, convert
+from bson import ObjectId
+
+
+
+
